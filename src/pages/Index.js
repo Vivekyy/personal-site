c5e61a673a34c10adc1c7c89512239087e340343
Vivek Yanamadula <vivekyanamadula@gmail.com>
2021-05-18 02:36:56 -0700

    More simple changes, finished sidebar and header

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -10,18 +10,16 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">Quick Links</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            Links to other sites and social media here.
           </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
+      <p> Please feel free to read more <Link to="/about">about me</Link>,
+        check out my {' '}
+        <Link to="/resume">resume</Link> {' '}
+        and <Link to="/projects">projects</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
     </article>
